6acadfa7f5581d41c47c53f40ba8cbfa664e4794
rb989 <wow67501@gmail.com>
2023-08-10 21:08:35 -0400

    Fix constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,6 @@
 const constants = {
   mapboxToken: import.meta.env.VITE_MAPBOX_TOKEN,
   baseApiUrl: import.meta.env.VITE_API_BASE_URL,
-  getRoutesUrl: `${import.meta.env.VITE_API_BASE_URL}/all-routes`,
-  addLocationUrl: `${import.meta.env.VITE_API_BASE_URL}/locations`,
-  getRouteLocationsUrl: `${import.meta.env.VITE_API_BASE_URL}/route-locations`
 }
 
 export default constants;
